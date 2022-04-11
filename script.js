060d0c0bc4716132763966f14e12a6514489a35f
ianjustinferris <ianjustinferris@gmail.com>
2022-04-11 16:04:43 -0400

    Added 1 layer of Glaucomys Volans Occurance Data

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40],
-  zoom: 9
-});
+mapboxgl.accessToken =
+        "pk.eyJ1IjoiaWFuanVzdGluZmVycmlzIiwiYSI6ImNsMXUzdWFrdjI5YzEzY3BjcTN2bHdxcXkifQ.nHDp49alvjpiTFbRUmWL0Q";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/ianjustinferris/cl1v4u4th000314p76tksbira",
+        center: [-84.3880,33.7490],
+        zoom: 9
+      });
+
+
+// Add occurance Layer Flying Squirrel
+// map.on('load', () => {
+// map.addSource('ianjustinferris.7e7jfhov', {
+// type: 'vector',
+// url: 'mapbox://mapbox.ianjustinferris.7e7jfhov'
+// });
+// map.addLayer({
+// 'id': 'ianjustinferris.7e7jfhov',
+// 'type': 'fill',
+// 'source': 'ianjustinferris.7e7jfhov',
+// 'source-layer': 'roads',
+
+
+
+// });
+// });
+
